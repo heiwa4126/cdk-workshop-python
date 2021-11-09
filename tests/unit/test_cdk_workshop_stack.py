d37d0cfd10f3021d11686113b67c5a473500297a
Masato Ota <heiwa4126@gmail.com>
2021-11-09 11:04:52 +0900

    Initial commit

---
+++ tests/unit/test_cdk_workshop_stack.py
@@ -0,0 +1,24 @@
+from aws_cdk import (
+        core,
+        assertions
+    )
+
+from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
+
+
+def test_sqs_queue_created():
+    app = core.App()
+    stack = CdkWorkshopStack(app, "cdk-workshop")
+    template = assertions.Template.from_stack(stack)
+
+    template.has_resource_properties("AWS::SQS::Queue", {
+        "VisibilityTimeout": 300
+    })
+
+
+def test_sns_topic_created():
+    app = core.App()
+    stack = CdkWorkshopStack(app, "cdk-workshop")
+    template = assertions.Template.from_stack(stack)
+
+    template.resource_count_is("AWS::SNS::Topic", 1)
